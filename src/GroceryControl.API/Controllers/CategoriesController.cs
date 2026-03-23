2da0ab51ce172fb28864e6462f9c9fa75ef977cd
CasaTIC <instructor.proxicasatic@casatic.org>
2026-03-23 14:15:23 -0600

    feat: add POST /api/categories endpoint for creating new categories

--- src/GroceryControl.API/Controllers/CategoriesController.cs
+++ src/GroceryControl.API/Controllers/CategoriesController.cs
@@ -1,10 +1,15 @@
+using GroceryControl.Application.Common.Interfaces;
 using GroceryControl.Application.Features.Categories.DTOs;
 using GroceryControl.Application.Features.Categories.Queries.GetCategories;
+using GroceryControl.Domain.Entities;
+using GroceryControl.Domain.Enums;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GroceryControl.API.Controllers;
+
+public record CreateCategoryRequest(string Name);
 
 [ApiController]
 [Route("api/[controller]")]
@@ -12,10 +17,12 @@ namespace GroceryControl.API.Controllers;
 public class CategoriesController : ControllerBase
 {
     private readonly IMediator _mediator;
+    private readonly IApplicationDbContext _context;
 
-    public CategoriesController(IMediator mediator)
+    public CategoriesController(IMediator mediator, IApplicationDbContext context)
     {
         _mediator = mediator;
+        _context = context;
     }
 
     [HttpGet]
@@ -24,5 +31,23 @@ public class CategoriesController : ControllerBase
     {
         var result = await _mediator.Send(new GetCategoriesQuery(), ct);
         return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<CategoryDto>> Create(
+        [FromBody] CreateCategoryRequest request, CancellationToken ct)
+    {
+        var maxId = _context.Categories.Max(c => c.Id);
+        var category = new Category
+        {
+            Id = maxId + 1,
+            Name = request.Name,
+            DefaultUnitCategory = UnitCategory.Countable,
+            SortOrder = maxId + 1,
+        };
+        _context.Categories.Add(category);
+        await _context.SaveChangesAsync(ct);
+
+        return Ok(new CategoryDto(category.Id, category.Name, null, null, category.DefaultUnitCategory, category.SortOrder));
     }
 }
